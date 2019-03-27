c36a84702482da6f99d159bfcfc376cbd1f08e08
Kerry Snyder <krry@me.com>
2019-03-27 04:19:00 -0500

    implement scrolliner for posts

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,58 +5,89 @@
  */
 !function (name, definition) {
 
-  if (typeof module != 'undefined') module.exports = definition()
-  else if (typeof define == 'function' && typeof define.amd == 'object') define(definition)
-  else this[name] = definition()
+    if (typeof module != 'undefined') module.exports = definition();
+    else if (typeof define == 'function' && typeof define.amd == 'object') define(definition);
+    else this[name] = definition();
 
 }('domready', function () {
 
-  var fns = [], listener
-    , doc = typeof document === 'object' && document
-    , hack = doc && doc.documentElement.doScroll
-    , domContentLoaded = 'DOMContentLoaded'
-    , loaded = doc && (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+    var fns = [],
+        listener,
+        doc = typeof document === 'object' && document,
+        hack = doc && doc.documentElement.doScroll,
+        domContentLoaded = 'DOMContentLoaded',
+        loaded = doc && (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState);
 
 
-  if (!loaded && doc)
-    doc.addEventListener(domContentLoaded, listener = function () {
-      doc.removeEventListener(domContentLoaded, listener)
-      loaded = 1
-      while (listener = fns.shift()) listener()
-    })
+    if (!loaded && doc)
+        doc.addEventListener(domContentLoaded, listener = function () {
+            doc.removeEventListener(domContentLoaded, listener);
+            loaded = 1;
+            try {
+                listener = fns.shift();
+                listener();
+            } catch(error) {
+                console.error("could not shift because:", error);
+            }
+        });
 
-  return function (fn) {
-    loaded ? setTimeout(fn, 0) : fns.push(fn)
-  }
+    return function (fn) {
+        if (loaded) {
+            setTimeout(fn, 0);
+        } else {
+            fns.push(fn);
+        }
+    };
 
 });
 
 
 domready(function () {
-  console.info('DOM is content and loaded.');
+    console.info('DOM is content and loaded.');
 
-  // wire up the menu trigger button
-  document.querySelector('#menu_trigger').addEventListener('click', function(){
-    this.classList.toggle('triggered');
-    document.querySelector('#menu').classList.toggle('shown');
-  });
+    // wire up the menu trigger button
+    document.querySelector('#menu_trigger').addEventListener('click', function(){
+        this.classList.toggle('triggered');
+        document.querySelector('#menu').classList.toggle('shown');
+    });
 
-  // sense whether user navs with keyboard
-  window.addEventListener('keydown', handleFirstTab);
+    // sense whether user navs with keyboard
+    window.addEventListener('keydown', handleFirstTab);
 
-  // allow :active styles to work in CSS on mobile safari
-  document.addEventListener("touchstart", function(){}, true);
+    // allow :active styles to work in CSS on mobile safari
+    document.addEventListener("touchstart", function(){}, true);
 
-  // when the dom is loaded, fade in the page
-  document.body.classList.remove('dom-loading');
-  document.documentElement.style.backgroundColor="";
+    // when the dom is loaded, fade in the page
+    document.body.classList.remove('dom-loading');
+    document.documentElement.style.backgroundColor="";
+
+    var scrollPercent,
+        relevantHeight,
+        showScroll,
+        byline = document.querySelector('.byline'),
+        scrolliner = document.querySelector('.scrolliner'),
+        main = document.querySelector('main'),
+        post = document.querySelector('.post');
+
+    // display scroll progress in sticky header
+    main.addEventListener('scroll', function(){
+        bylineHeight = byline.offsetTop;
+        relevantHeight = post.offsetHeight - window.innerHeight;
+        scrollPercent = 100 * main.scrollTop / relevantHeight;
+        if (typeof scrolliner === "object") {
+            showScroll = main.scrollTop < bylineHeight;
+            scrolliner.classList.toggle("ghost", showScroll);
+            scrollinerPosition = -100 + scrollPercent;
+            scrolliner.style.transform = "translate3d(calc("+scrollinerPosition+"% - 4em), 0, 0)";
+        }
+    });
 });
 
 // if keyboard navver, leave the visual accommodations alone
 function handleFirstTab(e) {
-  if (e.keyCode === 9) { // the "I am a keyboard user" key
-    document.body.classList.add('user-is-tabbing');
-    window.removeEventListener('keydown', handleFirstTab);
-  }
+    if (e.keyCode === 9) { // the "I am a keyboard user" key
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
 }
 
