352581d6bf338e98f267f926c5b73bcd7b9851c3
Kerry Snyder <kerry@atmanaut.me>
2020-11-13 04:47:15 -0800

    fix search and spinner

--- assets/js/fastsearch.js
+++ assets/js/fastsearch.js
@@ -1,55 +1,55 @@
-var fuse; // holds our search engine
-var searchVisible = false;
-var firstRun = true; // allow us to delay loading json data unless search activated
-var resultsAvailable = false; // Did we get any search results?
-var searchResults = document.getElementById("searchResults"); // targets the <ul>
-var searchInput = document.getElementById("searchInput"); // input box for search
-var fastSearch = document.getElementById("fastSearch"); // input box for search
-var searchHint = document.getElementById("searchHint"); // input box for search
-var modkey = document.getElementById("modkey"); // input box for search
-var firstResult = searchResults.firstChild; // first child of search list
-var lastResult = searchResults.lastChild; // last child of search list
+var fuse // holds our search engine
+var searchVisible = false
+var firstRun = true // allow us to delay loading json data unless search activated
+var resultsAvailable = false // Did we get any search results?
+var searchResults = document.getElementById('searchResults') // targets the <ul>
+var searchInput = document.getElementById('searchInput') // input box for search
+var fastSearch = document.getElementById('fastSearch') // input box for search
+var searchHint = document.getElementById('searchHint') // input box for search
+var modkey = document.getElementById('modkey') // input box for search
+var firstResult = searchResults.firstChild // first child of search list
+var lastResult = searchResults.lastChild // last child of search list
 
 function showSearch() {
-  fastSearch.classList.add("shown"); // show search box
-  modkey.classList.add('shown');
-  searchInput.focus(); // put focus in input box so you can just start typing
-  searchVisible = true; // search visible
-  removeSearchShower();
-  addSearchHider();
+  fastSearch.classList.add('shown') // show search box
+  modkey.classList.add('shown')
+  searchInput.focus() // put focus in input box so you can just start typing
+  searchVisible = true // search visible
+  removeSearchShower()
+  addSearchHider()
 }
 
 function hideSearch() {
-  fastSearch.classList.remove("shown"); // hide search box
-  document.activeElement.blur(); // remove focus from search box
-  searchVisible = false; // search not visible
-  addSearchShower();
-  removeSearchHider();
+  fastSearch.classList.remove('shown') // hide search box
+  document.activeElement.blur() // remove focus from search box
+  searchVisible = false // search not visible
+  addSearchShower()
+  removeSearchHider()
 }
 
 function toggleSearch() {
-  return searchVisible ? hideSearch() : showSearch();
+  return searchVisible ? hideSearch() : showSearch()
 }
 
 function searchShower(event) {
   if (searchHint.contains(event.target)) {
-    checkSearch();
-    showSearch();
+    checkSearch()
+    showSearch()
   }
 }
 
-function searchHider(event){
+function searchHider(event) {
   if (!fastSearch.contains(event.target)) {
     hideSearch()
   }
 }
 
-function addSearchShower() {
+export function addSearchShower() {
   document.addEventListener('mouseup', searchShower)
 }
 
 function removeSearchShower() {
-  if (typeof(searchShower) !== "undefined") {
+  if (typeof searchShower !== 'undefined') {
     document.removeEventListener('mouseup', searchShower)
   }
 }
@@ -59,23 +59,19 @@ function addSearchHider() {
 }
 
 function removeSearchHider() {
-  if (typeof(searchHider) !== "undefined") {
+  if (typeof searchHider !== 'undefined') {
     document.removeEventListener('mouseup', searchHider)
   }
 }
 
 function checkSearch() {
   // loads our json data and builds fuse.js search index
-  return firstRun ? loadSearch() : (firstRun = false); // only one first run per page load
+  return firstRun ? loadSearch() : (firstRun = false) // only one first run per page load
 }
-// ==========================================
-// The main keyboard event listener running the show
-// and the initial mouse listener
-addSearchShower()
-document.addEventListener("keydown", function (event) {
+export function handleSearchKeys(event) {
   // CMD-P to show / hide Search
-  if ((event.metaKey && event.key === "p") || event.key === "Escape") {
-    event.preventDefault();
+  if ((event.metaKey && event.key === 'p') || event.key === 'Escape') {
+    event.preventDefault()
     // Load json search index if first time invoking search
     // Means we don't load json unless searches are going to happen; keep user payload small unless needed
     checkSearch()
@@ -84,66 +80,58 @@ document.addEventListener("keydown", function (event) {
   }
 
   // DOWN (40) arrow
-  if (event.key === "ArrowDown") {
+  if (event.key === 'ArrowDown') {
     if (searchVisible && resultsAvailable) {
-      event.preventDefault(); // stop window from scrolling
+      event.preventDefault() // stop window from scrolling
       if (document.activeElement === searchInput) {
-        firstResult.focus();
+        firstResult.focus()
       } // if the currently focused element is the main input --> focus the first <li>
       else if (document.activeElement === lastResult) {
-         // if we're at the bottom, stay there
-        lastResult.focus();
-      }
-      else {
+        // if we're at the bottom, stay there
+        lastResult.focus()
+      } else {
         // otherwise select the next search result
-        var nextItem = document.activeElement.parentElement.nextSibling.firstElementChild;
-        nextItem.focus();
-        nextItem.scrollIntoView({behavior: "smooth", block: "center"});
+        var nextItem = document.activeElement.parentElement.nextSibling.firstElementChild
+        nextItem.focus()
+        nextItem.scrollIntoView({behavior: 'smooth', block: 'center'})
       }
     }
   }
 
   // UP (38) arrow
-  if (event.key === "ArrowUp") {
+  if (event.key === 'ArrowUp') {
     if (searchVisible && resultsAvailable) {
-      event.preventDefault(); // stop window from scrolling
+      event.preventDefault() // stop window from scrolling
       if (document.activeElement === searchInput) {
-        searchInput.focus();
+        searchInput.focus()
       } // If we're in the input box, do nothing
       else if (document.activeElement === firstResult) {
-        searchInput.focus();
+        searchInput.focus()
       } // If we're at the first item, go to input box
       else {
-        var prevItem = document.activeElement.parentElement.previousSibling.firstElementChild;
-        prevItem.focus();
-        prevItem.scrollIntoView({behavior: "smooth", block: "center"});
+        var prevItem = document.activeElement.parentElement.previousSibling.firstElementChild
+        prevItem.focus()
+        prevItem.scrollIntoView({behavior: 'smooth', block: 'center'})
       } // Otherwise, select the search result above the current active one
     }
   }
-});
-
-// ==========================================
-// execute search as each character is typed
-//
-document.getElementById("searchInput").onkeyup = function (e) {
-  executeSearch(this.value);
-};
+}
 
 // ==========================================
 // fetch some json without jquery
 //
 function fetchJSONFile(path, callback) {
-  var httpRequest = new XMLHttpRequest();
+  var httpRequest = new XMLHttpRequest()
   httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState === 4) {
       if (httpRequest.status === 200) {
-        var data = JSON.parse(httpRequest.responseText);
-        if (callback) callback(data);
+        var data = JSON.parse(httpRequest.responseText)
+        if (callback) callback(data)
       }
     }
-  };
-  httpRequest.open("GET", path);
-  httpRequest.send();
+  }
+  httpRequest.open('GET', path)
+  httpRequest.send()
 }
 
 // ==========================================
@@ -151,10 +139,10 @@ function fetchJSONFile(path, callback) {
 // on first call of search box (CMD-/)
 //
 function loadSearch(e) {
-  fetchJSONFile("/index.json", function (data) {
+  fetchJSONFile('/index.json', function (data) {
     var options = {
       // fuse.js options; check fuse.js website for details
-      keys: ["title", "permalink", "contents", "tags", "subtitle", "lead"],
+      keys: ['title', 'permalink', 'contents', 'tags', 'subtitle', 'lead'],
       minMatchCharLength: 1,
       shouldSort: true,
       // ===the fuzzy search magic===
@@ -162,11 +150,11 @@ function loadSearch(e) {
       // to be within 800 characters of the `location`
       // to be found using a `threshold` of 0.8
       location: 0,
-      threshold: 0.3,
-      distance: 100,
-    };
-    fuse = new Fuse(data, options); // build the index from the json file
-  });
+      threshold: 0.5,
+      distance: 500,
+    }
+    fuse = new Fuse(data, options) // build the index from the json file
+  })
   return e
 }
 
@@ -176,28 +164,26 @@ function loadSearch(e) {
 // in the search box
 //
 function executeSearch(term) {
-  let results = fuse.search(term); // the actual query being run using fuse.js
-  let searchitems = ""; // our results bucket
+  let results = fuse.search(term) // the actual query being run using fuse.js
+  let searchitems = '' // our results bucket
 
   if (results.length === 0) {
     // no results based on what was typed into the input box
-    resultsAvailable = false;
-    searchitems = "";
+    resultsAvailable = false
+    searchitems = ''
   } else {
     // build our html
-    let tags = "";
+    let tags = ''
     for (let item in results.slice(0, 7)) {
       // only show first 5 results
-      // weed out duplicates
-      for (let s in searchitems) {
-        if (searchitems.includes(results[item].item.title)) {
-          continue;
-        }
+      if (searchitems.includes(results[item].item.title)) {
+        // weed out duplicates
+        continue
       }
-      const tagsArr = results[item].item.tags;
+      const tagsArr = results[item].item.tags
       for (let tag in tagsArr) {
-        tags = "#" + tagsArr[tag] + " " + tags;
-        if (tag > 5) break;
+        tags = '#' + tagsArr[tag] + ' ' + tags
+        if (tag > 5) break
       }
       searchitems =
         searchitems +
@@ -209,17 +195,20 @@ function executeSearch(term) {
         '</h2><p class="results-deets"><time class="results-date">' +
         results[item].item.date +
         '</time><br/><p>' +
-        results[item].item.contents.slice(0, 200) + "…" +
+        results[item].item.contents.slice(0, 200) +
+        '…' +
         '</p><span class="results-tags">' +
         tags +
-        "</span></p></a></li>";
+        '</span></p></a></li>'
     }
-    resultsAvailable = true;
+    resultsAvailable = true
   }
 
-  document.getElementById("searchResults").innerHTML = searchitems;
+  document.getElementById('searchResults').innerHTML = searchitems
   if (results.length > 0) {
-    firstResult = searchResults.firstChild.firstElementChild; // first result container — used for checking against keyboard up/down location
-    lastResult = searchResults.lastChild.firstElementChild; // last result container — used for checking against keyboard up/down location
+    firstResult = searchResults.firstChild.firstElementChild // first result container — used for checking against keyboard up/down location
+    lastResult = searchResults.lastChild.firstElementChild // last result container — used for checking against keyboard up/down location
   }
 }
+
+export default executeSearch
